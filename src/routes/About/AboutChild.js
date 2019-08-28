949d0e48b29bfda3c3a12248b7d60d57fcf8f306
CcXxWw112233 <1109567482@qq.comm>
2019-08-28 22:43:43 +0800

    '二级路由实现'

---
+++ src/routes/About/AboutChild.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const AboutChild = () => {
+    return (
+        <div>
+            我是关于的内容详情
+        </div>
+    );
+}
+export default AboutChild
--- src/routes/About/index.js
+++ src/routes/About/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+import Topics from '../Topics'
+import AboutChild from './AboutChild'
 const About = () => (
     <div>
         about
+        <ul>
+            <li><Link to="/about/detail">去看看详情吧</Link></li>
+        </ul>
+        <Switch>
+            <Route path="/about/detail" component={AboutChild}/>
+        </Switch>
     </div>
 )
 
